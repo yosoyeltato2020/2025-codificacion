5142df6d669ac0138b923ab51f4e19b3663463e0
elMestre <carmesce@gmail.com>
2025-04-07 11:50:08 +0200

    ejemplo

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+def linea(num: int, qty: int = 5):
+    for i in range(qty):
+        code = num + i
+        print(f"{code}".rjust(3), end="")
+        print(": ", end="")
+        if code not in [9, 10, 13]:
+            print(chr(code), end="")
+        else:
+            print("*", end="")
+        print(" " * 3, end="")
+    print()
+
+
+def main(init: int = 32) -> None:
+    qty = 9
+    c = init
+    while c <= 250:
+        linea(c, qty)
+        c += qty
+
+
 if __name__ == "__main__":
-    print(chr(98))
-    print("=======")
-    print(ord('c'))
+    main(0)
