cfdb5c5f237a4e16fafcea24479c33b8184cd970
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-04-07 13:25:44 +0200

    he sustituido el codigo del Mestre por mi codigo

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-def linea(num: int, qty: int = 5):
-    for i in range(qty):
-        code = num + i
-        print(f"{code}".rjust(3), end="")
-        print(": ", end="")
-        if code not in [9, 10, 13]:
-            print(chr(code), end="")
-        else:
-            print("*", end="")
-        print(" " * 3, end="")
-    print()
-
-
-def main(init: int = 32) -> None:
-    qty = 9
-    c = init
-    while c <= 250:
-        linea(c, qty)
-        c += qty
-
-
-if __name__ == "__main__":
-    main(0)
+print("Códigos ASCII")
+for i in range(256):
+    if i % 6 == 0:
+        print() 
+        print()
+    if 32 <= i <= 255:
+        print(f"{str(i).ljust(15)}: {chr(i)}", end="\t")
+    else:
+        pass
+        # print(f"{str(i).ljust(20)}: no imprimible", end="\t")
