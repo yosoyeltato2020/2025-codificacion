b5251a8264efcbc5c70ee2760a0922ab1b28bf4c
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-04-10 10:14:05 +0200

    ahora hago una lista con diferentes caracteres y escoge aleatoriamente los caracteres de relleno

--- cuadricula.py
+++ cuadricula.py
@@ -1,4 +1,5 @@
 import os
+import random 
 Rojo = "\033[31m"
 verde = "\033[32m"
 blanco = "\033[37m"
@@ -8,10 +9,14 @@ dimensiones = {
     "lines": os.get_terminal_size().lines,
 }
 
+def obtener_caracteres():
+    lista = ["#", "*", "@", "$", "%", "&"]
+    return lista
 
 def dibujar_cuadricula():
     ancho = dimensiones["col"]
     alto = dimensiones["lines"]
+    caracteres_ascii = obtener_caracteres()
 
     for linea in range(0,alto):
         if linea == 0:
@@ -19,16 +24,13 @@ def dibujar_cuadricula():
             print(chr(9484) + chr(9472) * (ancho - 2) + chr(9488))
         elif linea == alto - 1:
            
-           print(chr(9492) + chr(9472) * (ancho - 2) + chr(9496)) 
+            print(chr(9492) + chr(9472) * (ancho - 2) + chr(9496)) 
         else:
-            if linea % 2 == 0:
-              
-                contenido = Rojo + "#" * (ancho - 2) + blanco
-            else:
-              
-                contenido = verde +  "*" * (ancho - 2) + blanco
+            contenido =""         
+            for _ in range(ancho - 2):  
+                contenido += random.choice(caracteres_ascii)  
+            print(chr(9474) + contenido + chr(9474))  
            
-            print(chr(9474) + contenido + chr(9474)) 
 
 def imprimir_registros(numero_registro):
     # Función para imprimir los registros enumerados
